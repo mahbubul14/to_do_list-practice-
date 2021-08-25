5353966cd5f722e18b2348d4e7bb9b00e9c20802
mahbubul14 <mahbub21alam2017@gmail.com>
2021-08-25 14:26:59 +0600

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
 
 const input = document.getElementById('input');
 const addBtn = document.getElementById('addBtn');
