5497c4b75f98de8285921b9e45024707ba1df18a
mahbubul14 <mahbub21alam2017@gmail.com>
2021-08-25 14:24:29 +0600

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -12,3 +12,21 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+const input = document.getElementById('input');
+const addBtn = document.getElementById('addBtn');
+const list = document.getElementById('list');
+
+addBtn.addEventListener('click', () => {
+  const newList = document.createElement('li');
+  newList.innerHTML = input.value;
+  list.appendChild(newList);
+
+  newList.addEventListener('click', () => {
+    newList.style.textDecoration = 'line-through';
+  });
+
+  newList.addEventListener('dblclick', () => {
+    list.removeChild(newList);
+  });
+});
